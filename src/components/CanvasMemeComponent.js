f9ac23be24037890e65712ca0c39283f2e59238b
Vlad <vladanhelov@gmail.com>
2023-03-10 23:26:51 +0200

    add canvas component

---
+++ src/components/CanvasMemeComponent.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+
+export default function CanvasMemeComponent(props) {
+  const { canvasRef, meme, topTextPosition, bottomTextPosition } = props;
+  const [canvasWidth, setCanvasWidth] = useState(0);
+
+  useEffect(() => {
+    const container = document.querySelector(".meme--image");
+    if (!container) {
+      console.error(".meme--image selector did not match any elements");
+      return;
+    }
+    setCanvasWidth(container.offsetWidth);
+    const handleResize = () => setCanvasWidth(container.offsetWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (!canvasRef.current) {
+      return;
+    }
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    const image = new Image();
+    image.onload = () => {
+      //треба переробити цей код, і зробити щоб текст на картинці співподав з текстом на канвасі
+      const container = document.querySelector(".meme--image");
+      canvas.width = container.offsetWidth;
+      canvas.height = container.offsetHeight;
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(image, 0, 0, canvas.width, canvas.height);
+      context.fillStyle = "#ffffff";
+      context.shadowBlur = 5;
+      context.shadowColor = "#000";
+      context.shadowOffsetX = 2;
+      context.shadowOffsetY = 2;
+      if (canvas.width < 500) {
+        context.font = "20px Impact";
+      } else {
+        context.font = "40px Impact";
+      }
+      const topX = Math.round(parseInt(topTextPosition.x));
+      const topY = Math.round(parseInt(topTextPosition.y));
+      const bottomX = Math.round(parseInt(bottomTextPosition.x));
+      const bottomY = Math.round(parseInt(bottomTextPosition.y));
+      const topTextX = (topX * canvas.width) / image.naturalWidth;
+      const topTextY = (topY * canvas.height) / image.naturalHeight;
+      context.fillText(meme.topText.toUpperCase(), topTextX, topTextY);
+      const bottomTextX = (bottomX * canvas.width) / image.naturalWidth;
+      const bottomTextY = (bottomY * canvas.height) / image.naturalHeight;
+      context.fillText(meme.bottomText.toUpperCase(), bottomTextX, bottomTextY);
+    };
+
+    image.src = meme.randomImage;
+    image.setAttribute("crossorigin", "anonymous");
+  }, [canvasWidth, meme, topTextPosition, bottomTextPosition]);
+  return <canvas ref={canvasRef} className="canvas--meme" />;
+}
--- src/components/DownloadMemeComponent.js
+++ src/components/DownloadMemeComponent.js
@@ -1,63 +1,12 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef } from "react";
+import CanvasMemeComponent from "./CanvasMemeComponent.js";
 
-export default function DownloadMemeComponent(props) {
-  const { meme, topTextPosition, bottomTextPosition } = props;
+export default function DownloadMemeComponent({
+  meme,
+  topTextPosition,
+  bottomTextPosition,
+}) {
   const canvasRef = useRef(null);
-  const [canvasWidth, setCanvasWidth] = useState(0);
-
-  useEffect(() => {
-    const container = document.querySelector(".meme--image");
-    if (!container) {
-      console.error(".meme--image selector did not match any elements");
-      return;
-    }
-    setCanvasWidth(container.offsetWidth);
-    const handleResize = () => setCanvasWidth(container.offsetWidth);
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  useEffect(() => {
-    if (!canvasRef.current) {
-      return;
-    }
-    const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
-
-    const image = new Image();
-    image.onload = () => {
-      //треба переробити цей код, і зробити щоб текст на картинці співподав з текстом на канвасі
-      const container = document.querySelector(".meme--image");
-      canvas.width = container.offsetWidth;
-      canvas.height = container.offsetHeight;
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      context.drawImage(image, 0, 0, canvas.width, canvas.height);
-      context.fillStyle = "#ffffff";
-      context.shadowBlur = 5;
-      context.shadowColor = "#000";
-      context.shadowOffsetX = 2;
-      context.shadowOffsetY = 2;
-      if (canvas.width < 500) {
-        context.font = "20px Impact";
-      } else {
-        context.font = "40px Impact";
-      }
-      const topX = Math.round(parseInt(topTextPosition.x));
-      const topY = Math.round(parseInt(topTextPosition.y));
-      const bottomX = Math.round(parseInt(bottomTextPosition.x));
-      const bottomY = Math.round(parseInt(bottomTextPosition.y));
-      const topTextX = (topX * canvas.width) / image.naturalWidth;
-      const topTextY = (topY * canvas.height) / image.naturalHeight;
-      context.fillText(meme.topText.toUpperCase(), topTextX, topTextY);
-      const bottomTextX = (bottomX * canvas.width) / image.naturalWidth;
-      const bottomTextY = (bottomY * canvas.height) / image.naturalHeight;
-      context.fillText(meme.bottomText.toUpperCase(), bottomTextX, bottomTextY);
-    };
-
-    image.src = meme.randomImage;
-    image.setAttribute("crossorigin", "anonymous");
-  }, [canvasWidth, meme, topTextPosition, bottomTextPosition]);
-
   const handleDownloadClick = () => {
     const canvas = canvasRef.current;
     const dataURL = canvas.toDataURL("image/png");
@@ -71,7 +20,12 @@ export default function DownloadMemeComponent(props) {
 
   return (
     <div className="form">
-      <canvas ref={canvasRef} className="canvas--meme" />
+      <CanvasMemeComponent
+        meme={meme}
+        topTextPosition={topTextPosition}
+        bottomTextPosition={bottomTextPosition}
+        canvasRef={canvasRef}
+      />
       <button className="download--button" onClick={handleDownloadClick}>
         Download Meme
       </button>
