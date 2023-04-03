b1f335a39c2d973f4c30c9c0f7daf1030f88cef8
Vlad <vladanhelov@gmail.com>
2023-04-03 20:33:42 +0300

    update style header and footer

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./MemeGenerator.css";
 import MemeHeader from "./components/MemeHeader.js";
 import MemeMain from "./components/MemeMain.js";
+import MemeFooter from "./components/MemeFooter";
 
 function MemeGenerator() {
   return (
     <div className="container">
       <MemeHeader />
       <MemeMain />
+      <MemeFooter />
     </div>
   );
 }
---
+++ src/components/MemeFooter.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faLinkedinIn,
+  faTelegram,
+} from "@fortawesome/free-brands-svg-icons";
+
+export default function MemeFooter(second) {
+  const handleMouseEnter = (e) => {
+    e.currentTarget.style.transform = "scale(1.3)";
+  };
+
+  const handleMouseLeave = (e) => {
+    e.currentTarget.style.transform = "scale(1)";
+  };
+  return (
+    <footer className="footer">
+      <p className="social--text">Get in touch:</p>
+      <div className="social-icons">
+        <a
+          href="https://www.facebook.com/vladanhelov"
+          target="_blank"
+          className="icon facebook"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <FontAwesomeIcon icon={faFacebookF} />
+        </a>
+        <a
+          href="https://t.me/sumne_hivno"
+          target="_blank"
+          className="icon telegram"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <FontAwesomeIcon icon={faTelegram} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/vladanhelov/"
+          target="_blank"
+          className="icon linkedin"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <FontAwesomeIcon icon={faLinkedinIn} />
+        </a>
+      </div>
+    </footer>
+  );
+}
