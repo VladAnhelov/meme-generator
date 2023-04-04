6b499ad0997cc6570bca41b28e0782e6d0f99622
Vlad <vladanhelov@gmail.com>
2023-04-04 11:23:04 +0300

    add preloder for world block

--- src/components/MemePreviewBlock.js
+++ src/components/MemePreviewBlock.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Preloader from "./PreloaderComponent.js";
 
 export default function MemePreviewBlock(props) {
+  const [loadedImages, setLoadedImages] = React.useState([]);
   const url = "https://api.imgflip.com/get_memes";
+
+  const handleImageLoad = (index) => {
+    setLoadedImages((prevLoadedImages) => [...prevLoadedImages, index]);
+  };
 
   React.useEffect(() => {
     if (!props.worldMemesLoaded) {
@@ -26,12 +32,15 @@ export default function MemePreviewBlock(props) {
     <div className="meme-preview">
       {props.allMemeImages.map((image, index) => (
         <div className="im" key={index}>
+          {!loadedImages.includes(index) && <Preloader />}
           <img
             src={image.url}
             className="im--preview"
             crossOrigin="anonymous"
             alt=""
             onClick={() => handleClick(image.url)}
+            onLoad={() => handleImageLoad(index)}
+            style={{ display: loadedImages.includes(index) ? "block" : "none" }}
           />
         </div>
       ))}
