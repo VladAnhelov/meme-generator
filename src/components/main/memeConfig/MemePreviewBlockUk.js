bf9108f8772c25449bf67ad0b8bba7530d8f8e67
Vlad <vladanhelov@gmail.com>
2023-04-20 21:27:06 +0300

    return moment

--- src/components/main/memeConfig/MemePreviewBlockUk.js
+++ src/components/main/memeConfig/MemePreviewBlockUk.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Preloader from "./PreloaderComponent.js";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
@@ -6,7 +6,6 @@ import styles from "./MemePreviewBlock.module.scss";
 
 export default function MemePreviewBlockUk(props) {
   const [loadedImages, setLoadedImages] = React.useState([]);
-  const [ukMemesLoaded, setUkMemesLoaded] = useState(false);
   const url = "https://api.imgur.com/3/account/me/images";
   const token = "02a7154b1bb6296911f0a2de6031103f4b731fb2";
 
@@ -15,24 +14,21 @@ export default function MemePreviewBlockUk(props) {
   };
 
   React.useEffect(() => {
-    if (!ukMemesLoaded) {
-      fetch(url, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
+    fetch(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.data) {
+          props.setAllMemeImages(result.data);
+        } else {
+          console.log("No data found");
+        }
       })
-        .then((res) => res.json())
-        .then((result) => {
-          if (result.data) {
-            props.setAllMemeImages(result.data);
-            setUkMemesLoaded(true);
-          } else {
-            console.log("No data found");
-          }
-        })
-        .catch((error) => console.error(error));
-    }
-  }, [ukMemesLoaded, props]);
+      .catch((error) => console.error(error));
+  }, [props]);
 
   const handleClick = (imageUrl) => {
     props.setMeme((prevMeme) => ({
