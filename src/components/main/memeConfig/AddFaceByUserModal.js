134494b6cc17fa70927a43412cb3ff4db8923e7a
Vlad <vladanhelov@gmail.com>
2023-05-22 20:23:37 +0300

    fix

--- src/components/main/memeConfig/AddFaceByUserModal.js
+++ src/components/main/memeConfig/AddFaceByUserModal.js
@@ -2,24 +2,20 @@ import React from "react";
 import styles from "./AddFaceByUserModal.module.scss";
 import CanvasUserFace from "../../canvas/CanvasUserFace.js";
 import CanvasZoomControls from "../../canvas/CanvasZoomControls.js";
+import LoginModal from "./LoginModal.js";
 
 export default function AddFaceByUserModal({ addImageToCanvas }) {
   const [showModal, setShowModal] = React.useState(false);
   const [editedImage, setEditedImage] = React.useState(null);
-
   const [isSaveButtonDisabled, setIsSaveButtonDisabled] = React.useState(false);
-
   const [image, setImage] = React.useState(null);
-
   const [isCanvasVisible, setIsCanvasVisible] = React.useState(true);
-
   const [lastTouchTime, setLastTouchTime] = React.useState(0);
-
   const [isErasing, setIsErasing] = React.useState(false);
   const [isEraserIconActive, setIsEraserIconActive] = React.useState(false);
   const [isSaveIconActive, setIsSaveIconActive] = React.useState(false);
-
   const canvasUserFaceRef = React.useRef(null);
+  const [isSignedIn, setIsSignedIn] = React.useState(false);
 
   const handleTouchStart = (e) => {
     const currentTime = new Date().getTime();
@@ -92,127 +88,131 @@ export default function AddFaceByUserModal({ addImageToCanvas }) {
           <div className={styles.block_add}></div>
         </button>
       </div>
-      <div
-        className={`${styles.remove_bg_modal} ${
-          showModal ? styles.modal_show : ""
-        }`}
-      >
-        <div className={styles.remove_bg_content}>
-          <div className={styles.title_block}>
-            <p className={styles.title}>Add Your Face</p>
-            <p className={styles.title}>Click to Add</p>
-          </div>
-          <div className={styles.upload_and_preview_block}>
-            <label
-              className={styles.custom_file_upload}
-              onChange={handleImageUpload}
-            >
-              <input type="file" />
-              Upload
-            </label>
-            <div className={styles.edit_block}>
-              <div className={styles.edit_block_items}>
-                <div>
-                  <button
-                    className={styles.eraserButton}
-                    onClick={toggleEraser}
-                  >
-                    <img
-                      className={`${styles.eraserIcon} ${
-                        isErasing && isEraserIconActive
-                          ? styles.eraserIcon_active
-                          : ""
-                      }`}
-                      src="https://img.icons8.com/dusk/64/null/eraser.png"
-                      alt="eraser"
-                    />
-                  </button>
+      {showModal && !isSignedIn ? (
+        <LoginModal setShowModal={setShowModal} />
+      ) : (
+        <div
+          className={`${styles.remove_bg_modal} ${
+            showModal ? styles.modal_show : ""
+          }`}
+        >
+          <div className={styles.remove_bg_content}>
+            <div className={styles.title_block}>
+              <p className={styles.title}>Add Your Face</p>
+              <p className={styles.title}>Click to Add</p>
+            </div>
+            <div className={styles.upload_and_preview_block}>
+              <label
+                className={styles.custom_file_upload}
+                onChange={handleImageUpload}
+              >
+                <input type="file" />
+                Upload
+              </label>
+              <div className={styles.edit_block}>
+                <div className={styles.edit_block_items}>
+                  <div>
+                    <button
+                      className={styles.eraserButton}
+                      onClick={toggleEraser}
+                    >
+                      <img
+                        className={`${styles.eraserIcon} ${
+                          isErasing && isEraserIconActive
+                            ? styles.eraserIcon_active
+                            : ""
+                        }`}
+                        src="https://img.icons8.com/dusk/64/null/eraser.png"
+                        alt="eraser"
+                      />
+                    </button>
+                  </div>
+                  <div>
+                    <button
+                      className={styles.saveButton}
+                      onClick={handleSave}
+                      disabled={isSaveButtonDisabled}
+                    >
+                      <img
+                        className={`${styles.saveIcon} ${
+                          isSaveIconActive ? styles.saveIcon_active : ""
+                        }`}
+                        src="https://img.icons8.com/dusk/64/null/save--v1.png"
+                        alt="save"
+                      />
+                    </button>
+                  </div>
                 </div>
                 <div>
-                  <button
-                    className={styles.saveButton}
-                    onClick={handleSave}
-                    disabled={isSaveButtonDisabled}
-                  >
-                    <img
-                      className={`${styles.saveIcon} ${
-                        isSaveIconActive ? styles.saveIcon_active : ""
-                      }`}
-                      src="https://img.icons8.com/dusk/64/null/save--v1.png"
-                      alt="save"
+                  <div>
+                    <CanvasZoomControls
+                      onZoomIn={handleZoomIn}
+                      onZoomOut={handleZoomOut}
                     />
-                  </button>
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <button
+                      className={styles.undoButton}
+                      onClick={() => canvasUserFaceRef.current.undo()}
+                    >
+                      <img
+                        className={styles.undoIcon}
+                        src="https://img.icons8.com/dusk/64/null/reply-arrow.png"
+                        alt="back"
+                      />
+                    </button>
+                    <button
+                      className={styles.redoButton}
+                      onClick={() => canvasUserFaceRef.current.redo()}
+                    >
+                      <img
+                        className={styles.redoIcon}
+                        src="https://img.icons8.com/dusk/64/null/forward-arrow.png"
+                        alt="forward"
+                      />
+                    </button>
+                  </div>
                 </div>
               </div>
-              <div>
-                <div>
-                  <CanvasZoomControls
-                    onZoomIn={handleZoomIn}
-                    onZoomOut={handleZoomOut}
+
+              {isCanvasVisible ? (
+                <div className={styles.canvasPreview}>
+                  <CanvasUserFace
+                    ref={canvasUserFaceRef}
+                    src={image}
+                    isErasing={isErasing}
                   />
                 </div>
-              </div>
-              <div>
-                <div>
-                  <button
-                    className={styles.undoButton}
-                    onClick={() => canvasUserFaceRef.current.undo()}
-                  >
-                    <img
-                      className={styles.undoIcon}
-                      src="https://img.icons8.com/dusk/64/null/reply-arrow.png"
-                      alt="back"
-                    />
-                  </button>
-                  <button
-                    className={styles.redoButton}
-                    onClick={() => canvasUserFaceRef.current.redo()}
-                  >
-                    <img
-                      className={styles.redoIcon}
-                      src="https://img.icons8.com/dusk/64/null/forward-arrow.png"
-                      alt="forward"
-                    />
-                  </button>
-                </div>
-              </div>
-            </div>
-
-            {isCanvasVisible ? (
-              <div className={styles.canvasPreview}>
-                <CanvasUserFace
-                  ref={canvasUserFaceRef}
-                  src={image}
-                  isErasing={isErasing}
+              ) : editedImage ? (
+                <img
+                  src={editedImage}
+                  className={styles.userFacePreview}
+                  onDragStart={handleDragStart}
+                  onClick={addImgClick}
+                  onDoubleClick={handleImageDoubleClick}
+                  onTouchStart={handleTouchStart}
+                  alt=""
                 />
-              </div>
-            ) : editedImage ? (
-              <img
-                src={editedImage}
-                className={styles.userFacePreview}
-                onDragStart={handleDragStart}
-                onClick={addImgClick}
-                onDoubleClick={handleImageDoubleClick}
-                onTouchStart={handleTouchStart}
-                alt=""
-              />
-            ) : (
-              <img
-                src={image}
-                className={styles.userFacePreview}
-                onDragStart={handleDragStart}
-                onClick={addImgClick}
-                onDoubleClick={handleImageDoubleClick}
-                onTouchStart={handleTouchStart}
-                alt=""
-              />
-            )}
+              ) : (
+                <img
+                  src={image}
+                  className={styles.userFacePreview}
+                  onDragStart={handleDragStart}
+                  onClick={addImgClick}
+                  onDoubleClick={handleImageDoubleClick}
+                  onTouchStart={handleTouchStart}
+                  alt=""
+                />
+              )}
+            </div>
+            <button className={styles.close_btn} onClick={handleClick}>
+              X
+            </button>
           </div>
-          <button className={styles.close_btn} onClick={handleClick}>
-            X
-          </button>
         </div>
-      </div>
+      )}
     </>
   );
 }
--- src/components/main/memeConfig/LoginModal.js
+++ src/components/main/memeConfig/LoginModal.js
@@ -1,12 +1,23 @@
 import React from "react";
-import NavBarMenu from "../../header/NavBarMenu";
 import loginModalStyle from "./LoginModal.module.scss";
 
-export default function LoginModal() {
+export default function LoginModal({ setShowModal }) {
+  const handleClick = (e) => {
+    setShowModal(false);
+  };
+
   return (
-    <div className={loginModalStyle.block}>
-      <p>This feature works only if you </p>
-      <NavBarMenu />
-    </div>
+    <>
+      <div className={loginModalStyle.block}>
+        <div className={loginModalStyle.remove_bg_modal}>
+          <div className={loginModalStyle.login_warning_content}>
+            <p>This feature works only if you sign up or sign in</p>
+            <button className={loginModalStyle.close_btn} onClick={handleClick}>
+              X
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
