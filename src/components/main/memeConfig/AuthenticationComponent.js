0d2b84a8c2781c0394af1a9e35dbee1b84e051d1
Vlad <vladanhelov@gmail.com>
2023-05-25 14:43:46 +0300

    add simple validation

--- src/components/main/memeConfig/AuthenticationComponent.js
+++ src/components/main/memeConfig/AuthenticationComponent.js
@@ -5,26 +5,55 @@ import styles from "../../header/NavBarMenu.module.scss";
 export default function AuthenticationComponent({ onClose, onSignIn }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const validateEmail = () => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
+
+  const validatePassword = () => {
+    return password.length >= 8;
+  };
 
   const handleSignIn = async (event) => {
     event.preventDefault();
 
-    try {
-      const { user } = await signInWithEmailAndPassword(auth, email, password);
-      alert("Signed in successfully.");
+    if (!validateEmail()) {
+      setEmailError("Please enter a valid email.");
+    } else {
+      setEmailError("");
+    }
 
-      const userToStore = {
-        displayName: user.displayName,
-        email: user.email,
-        uid: user.uid,
-      };
+    if (!validatePassword()) {
+      setPasswordError("Password must be at least 6 characters long.");
+    } else {
+      setPasswordError("");
+    }
 
-      onSignIn(userToStore);
-      localStorage.setItem("user", JSON.stringify(userToStore));
-      window.location.reload();
-      onClose();
-    } catch (error) {
-      alert(`Error signing in: ${error.message}`);
+    if (validateEmail() && validatePassword()) {
+      try {
+        const { user } = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password,
+        );
+        alert("Signed in successfully.");
+
+        const userToStore = {
+          displayName: user.displayName,
+          email: user.email,
+          uid: user.uid,
+        };
+
+        onSignIn(userToStore);
+        localStorage.setItem("user", JSON.stringify(userToStore));
+        window.location.reload();
+        onClose();
+      } catch (error) {
+        alert(`Error signing in: ${error.message}`);
+      }
     }
   };
 
@@ -36,19 +65,29 @@ export default function AuthenticationComponent({ onClose, onSignIn }) {
           <input
             type="email"
             name="email"
-            className={styles.formControl}
+            className={`${styles.formControl} ${
+              emailError ? styles.error : ""
+            }`}
             placeholder="Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+          {emailError && (
+            <div className={styles.errorMessage}>{emailError}</div>
+          )}
           <input
             type="password"
             name="password"
-            className={styles.formControl}
+            className={`${styles.formControl} ${
+              passwordError ? styles.error : ""
+            }`}
             placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          {passwordError && (
+            <div className={styles.errorMessage}>{passwordError}</div>
+          )}
           <button type="submit" className={styles.submitBtn}>
             Sign In
           </button>
