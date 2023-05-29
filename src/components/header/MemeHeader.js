81adef83360d57e55cb33f549325a34261c13696
Vlad <vladanhelov@gmail.com>
2023-05-29 14:03:32 +0300

    change color

--- src/components/header/MemeHeader.js
+++ src/components/header/MemeHeader.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import NavBarMenu from "./NavBarMenu.js";
 import styles from "./MemeHeader.module.scss";
+import { ThemeContext } from "../main/ThemeContext.js"; // Імпортуйте ваш контекст тут
 
 export default function MemeHeader() {
+  const { isDarkTheme } = useContext(ThemeContext); // Використовуйте контекст тут
+
+  const headerNameStyle = {
+    color: isDarkTheme ? "black" : "white", // Змініть ці кольори на ті, які ви хочете використовувати для темної та світлої тем
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.block}>
@@ -11,9 +18,9 @@ export default function MemeHeader() {
           src="https://stock-image.s3.amazonaws.com/editted-logo-fav-2.png"
           alt=""
         />
-        <div className={styles.block_headerName}>
-          <h2 className={styles.headerTitle}>Memebulance</h2>
-          <h4 className={styles.headerProject}>Vlad Anhelov Project</h4>
+        <div className={styles.block_headerName} style={headerNameStyle}>
+          <h2>Memebulance</h2>
+          <h4>Vlad Anhelov Project</h4>
         </div>
       </div>
       <NavBarMenu />
