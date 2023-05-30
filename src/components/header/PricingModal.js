1f71581661fad4992095600d68340e234649aaee
Vlad <vladanhelov@gmail.com>
2023-05-30 10:58:31 +0300

    add pricing card

--- src/components/header/PricingModal.js
+++ src/components/header/PricingModal.js
@@ -34,10 +34,66 @@ export default function PricingModal({ show, onClose }) {
 
   return (
     <div className={style.aboutBlock} ref={modalRef}>
-      <p>Pricing</p>
-      <button className={style.close_btn} onClick={handleClose}>
-        X
-      </button>
+      <div className={style.grid}>
+        <div className={style.card}>
+          <button className={style.close_btn} onClick={handleClose}>
+            X
+          </button>
+          <h2 className={style.card_title}>Student</h2>
+          <p className={style.pricing}>
+            20$<span className={style.small}>/per month</span>
+          </p>
+          <p>Save $9</p>
+          <ul className={style.features}>
+            <li>One account</li>
+            <li>Unlimited memes</li>
+            <li>Add 2 faces</li>
+          </ul>
+          <a href="/" className={style.cta_btn}>
+            Buy Now
+          </a>
+        </div>
+      </div>
+      <div className={style.grid}>
+        <div className={style.card}>
+          <button className={style.close_btn} onClick={handleClose}>
+            X
+          </button>
+          <h2 className={style.card_title}>Personal</h2>
+          <p className={style.pricing}>
+            39$<span className={style.small}>/per month</span>
+          </p>
+          <p>Save $15</p>
+          <ul className={style.features}>
+            <li>One account</li>
+            <li>Unlimited memes</li>
+            <li>Add 10 faces </li>
+          </ul>
+          <a href="/" className={style.cta_btn}>
+            Buy Now
+          </a>
+        </div>
+      </div>
+      <div className={style.grid}>
+        <div className={style.card}>
+          <button className={style.close_btn} onClick={handleClose}>
+            X
+          </button>
+          <h2 className={style.card_title}>Family</h2>
+          <p className={style.pricing}>
+            60$<span className={style.small}>/per month</span>
+          </p>
+          <p>Save $25</p>
+          <ul className={style.features}>
+            <li>Six account</li>
+            <li>Unlimited memes</li>
+            <li>Add 20 faces</li>
+          </ul>
+          <a href="/" className={style.cta_btn}>
+            Buy Now
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
