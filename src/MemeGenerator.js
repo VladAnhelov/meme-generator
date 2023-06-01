c1721ff73ed7977ecaeeafedc0f6933be6c3c9c4
Vlad <vladanhelov@gmail.com>
2023-06-01 20:50:14 +0300

    change background and add some style

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -3,16 +3,13 @@ import "./MemeGenerator.scss";
 import MemeHeader from "./components/header/MemeHeader.js";
 import MemeMain from "./containers/main/MemeMain.js";
 import MemeFooter from "./components/footer/MemeFooter.js";
-import Background from "./containers/main/Background.js";
-import { ThemeProvider, ThemeContext } from "./utils/ThemeContext.js"; // Імпортуйте ваш ThemeProvider тут
+import { ThemeProvider } from "./utils/ThemeContext.js"; // Імпортуйте ваш ThemeProvider тут
 
 function MemeGenerator() {
-  const { isDarkTheme } = React.useContext(ThemeContext);
   return (
     <ThemeProvider>
       <div className="container">
         <MemeHeader />
-        <Background key={isDarkTheme} />
         <MemeMain />
         <MemeFooter />
       </div>
