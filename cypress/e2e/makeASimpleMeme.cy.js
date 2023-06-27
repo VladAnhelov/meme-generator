ad49b5d1f1030b337e9c4b57e687035fd05b25ab
Vlad <vladanhelov@gmail.com>
2023-06-27 09:57:54 +0300

    add auto test

--- cypress/e2e/makeASimpleMeme.cy.js
+++ cypress/e2e/makeASimpleMeme.cy.js
@@ -1,25 +1,33 @@
-const baseURL = "https://memebulance.netlify.app/";
-const PREVIEW_BLOCK_MEME = ".MemePreviewBlock_memePreview__AARvz";
-const PREVIEW_BLOCK_IMAGE = ".MemePreviewBlock_image__VEwYN";
-const ADD_TEXT_FORM = ".AddMoreMemeText_form__-LE4Y";
-const ADD_TEXT_FORM_INPUT = ".AddMoreMemeText_formInput__oF9Z1";
-const DOWNLOAD_MEME_BUTTON = ".DownloadMemeComponent_downloadBtn__9Qk74";
+/* eslint-disable cypress/unsafe-to-chain-command */
+import selectors from "../fixtures/selectors.json";
 
-describe("visit home page and make a meme", () => {
+const baseURL = "https://memebulance.netlify.app/";
+
+
+describe("Visit home page and make a meme", () => {
   beforeEach(() => {
     cy.visit(baseURL);
   });
 
-  it("make a simple meme", () => {
-    cy.get(PREVIEW_BLOCK_MEME).find(PREVIEW_BLOCK_IMAGE).eq(1).click();
+  it("should create a simple meme", () => {
+    const {
+      MEME_PREVIEW_BLOCK_SELECTOR,
+      IMAGE_SELECTOR,
+      ADD_TEXT_FORM_INPUT,
+      DOWNLOAD_MEME_BUTTON
+    } = selectors;
 
-    cy.get(ADD_TEXT_FORM)
-      .find(ADD_TEXT_FORM_INPUT)
-      .eq(0)
+    cy.get(MEME_PREVIEW_BLOCK_SELECTOR)
+      .find(IMAGE_SELECTOR)
+      .eq(1)
+      .click();
+
+      cy.get(ADD_TEXT_FORM_INPUT)
+      .eq(1)
       .click()
-      .type("Hello, cypress!");
+      .type("Hello, Cypress!");
+    
 
-    cy.wait(1000);
     cy.get(DOWNLOAD_MEME_BUTTON).click();
   });
 });
