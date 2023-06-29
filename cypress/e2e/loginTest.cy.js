1f5be53151b6974e5c3f181100e91631d42d4c41
Vlad <vladanhelov@gmail.com>
2023-06-29 10:14:23 +0300

    add simple test

--- cypress/e2e/loginTest.cy.js
+++ cypress/e2e/loginTest.cy.js
@@ -1,22 +1,50 @@
 import selectors from "../fixtures/selectors.json";
+import userCredentials from "../fixtures/userCredentials.json";
 
 const baseURL = "https://memebulance.netlify.app/";
 
 describe("Visit home page and sign in", () => {
   beforeEach(() => {
     cy.visit(baseURL);
+    cy.get(selectors.SIGNIN_BUTTON)
+    .click()
   });
 
   it("should check the signIn form", () => {
     const {
-      SIGNIN_BUTTON, 
       SIGN_FORM 
     } = selectors;
 
-    cy.get(SIGNIN_BUTTON)
-    .click()
-
     cy.get(SIGN_FORM).contains("Sign In")
 
+  });
+
+  it('should check input fields', () => {
+    const {
+      EMAIL_INPUT, PASSWORD_INPUT
+    } = selectors;
+
+    const {
+      email, password
+    } = userCredentials;
+    cy.get(EMAIL_INPUT).type(email);
+    cy.get(PASSWORD_INPUT).type(password);
+  });
+
+  it('should fill in input fields and submit the form', () => {
+    const {
+      SUBMIT_BUTTON
+    } = selectors;
+    cy.get(SUBMIT_BUTTON).click();
+  })
+
+  it('should display an error message for invalid email', () => {
+    const {
+      SUBMIT_BUTTON
+    } = selectors;
+    cy.get(SUBMIT_BUTTON).click();
+
+    cy.get(".NavBarMenu_errorMessage__adhjR")
+    cy.contains('Please enter a valid email.').should('exist');
   });
 });
