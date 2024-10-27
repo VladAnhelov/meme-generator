e29bf348f07f357333a8ad492db96571a770f97b
Vlad Anhelov <vladyslav.a@miro.com>
2024-10-27 14:22:45 +0100

    add describe

--- src/tests/example.spec.ts
+++ src/tests/example.spec.ts
@@ -7,46 +7,48 @@ const locators = new MemeMainLocators();
 const BASE_URL = "https://memebulance.netlify.app/";
 const EXPECTED_TITLE = "Memebulance";
 
-test.beforeEach(async ({ page }) => {
-  await page.goto(BASE_URL);
-});
+test.describe("MemeMain", { tag: "regression tests" }, () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(BASE_URL);
+  });
 
-test("I check title ", async ({ page }) => {
-  const title = await page.title();
-  expect(title).toBe(EXPECTED_TITLE);
-});
+  test("I check title ", async ({ page }) => {
+    const title = await page.title();
+    expect(title).toBe(EXPECTED_TITLE);
+  });
 
-test("I check top and bottom field input and download", async ({
-  page,
-  downloadDir,
-}) => {
-  await page.click(locators.topTextInputField);
-  await page.fill(locators.topTextInputField, "Hello");
-  const checkTopInput = await page.inputValue(locators.topTextInputField);
-  expect(checkTopInput).toBe("Hello");
+  test("I check top and bottom field input and download", async ({
+    page,
+    downloadDir,
+  }) => {
+    await page.click(locators.topTextInputField);
+    await page.fill(locators.topTextInputField, "Hello");
+    const checkTopInput = await page.inputValue(locators.topTextInputField);
+    expect(checkTopInput).toBe("Hello");
 
-  await page.click(locators.bottomInputField);
-  await page.fill(locators.bottomInputField, "World");
-  const checkBottomInput = await page.inputValue(locators.bottomInputField);
-  expect(checkBottomInput).toBe("World");
+    await page.click(locators.bottomInputField);
+    await page.fill(locators.bottomInputField, "World");
+    const checkBottomInput = await page.inputValue(locators.bottomInputField);
+    expect(checkBottomInput).toBe("World");
 
-  // start download
-  const [download] = await Promise.all([
-    page.waitForEvent("download", { timeout: 10000 }).catch((error) => {
-      console.error("download is not start:", error);
-      throw error;
-    }),
-    page.click(locators.downloadMemeButton),
-  ]);
+    // start download
+    const [download] = await Promise.all([
+      page.waitForEvent("download", { timeout: 10000 }).catch((error) => {
+        console.error("download is not start:", error);
+        throw error;
+      }),
+      page.click(locators.downloadMemeButton),
+    ]);
 
-  const suggestedFilename = download.suggestedFilename();
-  const filePath = path.join(downloadDir, suggestedFilename);
-  await download.saveAs(filePath);
+    const suggestedFilename = download.suggestedFilename();
+    const filePath = path.join(downloadDir, suggestedFilename);
+    await download.saveAs(filePath);
 
-  // check if file exists
-  const fileExists = fs.existsSync(filePath);
-  expect(fileExists).toBe(true);
+    // check if file exists
+    const fileExists = fs.existsSync(filePath);
+    expect(fileExists).toBe(true);
 
-  const stats = fs.statSync(filePath);
-  expect(stats.size).toBeGreaterThan(0);
+    const stats = fs.statSync(filePath);
+    expect(stats.size).toBeGreaterThan(0);
+  });
 });
