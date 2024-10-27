1ecc8d683dcd43374b422d56b5d7b8f8f00ba0e5
Vlad Anhelov <vladyslav.a@miro.com>
2024-10-27 13:39:33 +0100

    add check field

--- src/tests/MemeMainLocators.comp.ts
+++ src/tests/MemeMainLocators.comp.ts
@@ -2,8 +2,10 @@ import { AutotestId } from "./AutotestId";
 import { Base } from "./base";
 
 export class MemeMainLocators extends Base {
-  public readonly topTextButton = this.derivedByTestId(AutotestId.TOP_TEXT);
-  public readonly buttonText = this.derivedByTestId(AutotestId.BUTTON_TEXT);
+  public readonly topTextInputField = this.derivedByTestId(AutotestId.TOP_TEXT);
+  public readonly bottomInputField = this.derivedByTestId(
+    AutotestId.BUTTON_TEXT,
+  );
   public readonly addMoreMemeTextButton = this.derivedByTestId(
     AutotestId.ADD_MORE_MEME_TEXT_BUTTON,
   );
--- src/tests/example.spec.ts
+++ src/tests/example.spec.ts
@@ -3,23 +3,24 @@ import { MemeMainLocators } from "./MemeMainLocators.comp.ts";
 import fs from "fs";
 import path from "path";
 
-test("basic test", async ({ page }) => {
+test("I check title ", async ({ page }) => {
   await page.goto("https://memebulance.netlify.app/", { timeout: 60000 }); // 60 секунд
   await page.waitForSelector("body");
   const title = await page.title();
   expect(title).toBe("Memebulance");
 });
 
-test("test_buttons", async ({ page }) => {
+test("I check top and button field input", async ({ page }) => {
   const locators = new MemeMainLocators();
-
   await page.goto("https://memebulance.netlify.app/");
-  await page.click(locators.topTextButton);
 
-  await page.fill(locators.topTextButton, "Hello");
+  await page.click(locators.topTextInputField);
+  await page.fill(locators.topTextInputField, "Hello");
+  expect(locators.topTextInputField).toBe("Hello");
 
-  const textValue = await page.inputValue(locators.topTextButton);
-  expect(textValue).toBe("Hello");
+  await page.click(locators.bottomInputField);
+  await page.fill(locators.bottomInputField, "World");
+  expect(locators.bottomInputField).toBe("World");
 
   // set download path to downloads folder
   const downloadDir = path.resolve(process.cwd(), "src/tests/downloads");
