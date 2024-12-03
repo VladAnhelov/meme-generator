fd587edb1a5461a3212fd6f989982bc7495f926a
Vlad Anhelov <vladyslav.a@miro.com>
2024-12-03 11:01:16 +0100

    small fix

--- src/tests/AboutModal.spec.ts
+++ src/tests/AboutModal.spec.ts
@@ -4,11 +4,24 @@ import { AboutModalLocators } from "./AboutModalLocators.comp.ts";
 const locators = new AboutModalLocators();
 const name_text = "test Vlad";
 const feedback_text = name_text;
-const BASE_URL = "http://localhost:3000";
+let BASE_URL = "http://localhost:3000";
+
+async function getBaseURL() {
+  try {
+    const response = await fetch(`${BASE_URL}`, { method: "HEAD" });
+    if (response.ok) {
+      console.log("Localhost is available, using BASE_URL:", BASE_URL);
+      return BASE_URL;
+    }
+  } catch (error) {
+    console.warn("Localhost not available, switching to Netlify URL.");
+  }
+  return "https://memebulance.netlify.app/";
+}
 
 test.describe("About Modal", { tag: "@regression tests" }, () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto(BASE_URL);
+    await page.goto(await getBaseURL());
     await page.click(locators.about_button);
   });
 
